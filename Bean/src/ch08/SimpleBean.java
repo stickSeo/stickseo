2f08545acffcde5ae78132d5dc766b7a4f68ec42
jieunseo2031 <stick.seo@gmail.com>
2015-02-21 18:20:35 +0900

    빈즈 예제

---
+++ Bean/src/ch08/SimpleBean.java
@@ -0,0 +1,17 @@
+package ch08;
+
+public class SimpleBean {
+
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
