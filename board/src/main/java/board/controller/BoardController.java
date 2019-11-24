4d042640f5ea7bc4a42669c34abae3ccb4d8da11
user <user@DESKTOP-DRDGLUG>
2019-11-25 03:16:41 +0900

    게시판 SQL 및 페이지 추가, 부트스트랩 추가

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -16,13 +16,27 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
+	
+	@RequestMapping("/main")
+	public ModelAndView root() throws Exception{
+		
+		ModelAndView mv = new ModelAndView("/board/main");
+		
+//		List<BoardDto> list = boardService.selectBoardList();
+//		mv.addObject("list", list);
+		
+		return mv;
+		
+	}
+	
+	
 	@RequestMapping("/board/boardList.do")
 	public ModelAndView BoardList() throws Exception{
 		
 		ModelAndView mv = new ModelAndView("/board/boardList");
 		
-		List<BoardDto> list = boardService.selectBoardList();
-		mv.addObject("list", list);
+//		List<BoardDto> list = boardService.selectBoardList();
+//		mv.addObject("list", list);
 		
 		return mv;
 		
--- board/src/main/resources/mapper/board.sql
+++ board/src/main/resources/mapper/board.sql
@@ -1,9 +1,15 @@
-create table tb_Board (
+CREATE TABLE TB_BOARD(
+     boardIdx NUMBER PRIMARY KEY,
+	 title varchar2(10) NOT NULL,
+	 content varchar2(40) NOT NULL,
+	 hitCnt NUMBER default 0,
+	 creatorId varchar2(10),
+	 createdDatetime varchar2(10),
+	 updaterId varchar2(10),
+	 updatedDatetime varchar2(10),
+	 deleteId varchar2(10),
+	 deletedDatetime varchar2(10),
+	 deleted_yn varchar(2) default 'N'
+);
 
-
-
-
-
-
-)
-
+SELECT * FROM tb_Board;
